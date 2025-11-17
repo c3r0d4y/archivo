e09a95b2f5223fd2fba8d9f819b18c5515b6a0ea
C3r0d4y <ceroday3@gmail.com>
2025-11-16 22:32:11 -0600

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //Dos formas diferentes de declarar una constante
-    const  LBS = 'library/';
-    const VIEWS = 'views/';
-    define ('DFT', 'default');
-    define ('RC', 'resource');
-    define ('URL', 'http://127.0.0.1/archivo/');
-    define ('SALT', 'piensosinexistir');
-?>
